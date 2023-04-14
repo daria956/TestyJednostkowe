7201eae5f38c39ed8975603e72255b4add3f12a6
daria956 <125731362+daria956@users.noreply.github.com>
2023-04-14 21:44:14 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestyJednostkowe
+{
+
+    public class Calculator
+    {
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+        public int MultiplicationAndDivion(int a, int b, int c )
+        {
+            return (a * b) / c;
+
+        }
+    }
+
+
+}
